c87639c78966e7e0331fe4ed409b438080acd3ef
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-24 23:36:32 +0100

    method refactor to add a condicional for a simian dna, that should have four followed nitrogens bases equals

--- dnaChecker.js
+++ dnaChecker.js
@@ -1,13 +1,19 @@
 const isSimian = dna => {
   const nitrogenBasesPosibles = ["A", "T", "C", "G"]
+  let dnaResult = false
 
   dna.forEach(sequence => {
+    // console.log(sequence)
     const nitrogenBases = sequence.split("")
+    // console.log(nitrogenBases)
     nitrogenBases.forEach(base => {
-      if (nitrogenBases.length >= 4 && nitrogenBasesPosibles.includes(base)) return true
+      // console.log(base)
+      dnaResult = nitrogenBases.length >= 4 && nitrogenBasesPosibles.includes(base)
     })
   })
-  return false
+  return dnaResult
 }
+
+
 
 module.exports = { isSimian }
