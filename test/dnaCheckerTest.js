546078316cddc1192a0adc2c689d39360d6fbd85
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-24 23:03:11 +0100

    test refactor to apply KISS principle

--- test/dnaCheckerTest.js
+++ test/dnaCheckerTest.js
@@ -5,38 +5,39 @@ describe("SimiosOrHuman", function () {
   describe("isSimian", function () {
     it("should return false given a empty dna input", function () {
       const dna = []
-      const dnaResult = dnaChecker.isSimian(dna)
-      assert.strictEqual(dnaResult, false)
+      assert.strictEqual(dnaChecker.isSimian(dna), false)
     })
+
     it("should return false given a empty dna sequence", function () {
       const dna = [""]
-      const dnaResult = dnaChecker.isSimian(dna)
-      assert.strictEqual(dnaResult, false)
+      assert.strictEqual(dnaChecker.isSimian(dna), false)
     })
+
     it("should return false given a dna sequence with one nitrogen base", function () {
       const dna = ["T"]
-      const dnaResult = dnaChecker.isSimian(dna)
-      assert.strictEqual(dnaResult, false)
+      assert.strictEqual(dnaChecker.isSimian(dna), false)
     })
+
     it("should return false given a dna sequence with two nitrogen bases", function () {
       const dna = ["TC"]
-      const dnaResult = dnaChecker.isSimian(dna)
-      assert.strictEqual(dnaResult, false)
+      const dnaResult =
+        assert.strictEqual(dnaChecker.isSimian(dna), false)
     })
+
     it("should return false given a dna sequence with three nitrogen bases", function () {
       const dna = ["TCA"]
-      const dnaResult = dnaChecker.isSimian(dna)
-      assert.strictEqual(dnaResult, false)
+      assert.strictEqual(dnaChecker.isSimian(dna), false)
     })
+
     it("should return false given a dna sequence with four different nitrogen bases", function () {
       const dna = ["TCAG"]
-      const dnaResult = dnaChecker.isSimian(dna)
-      assert.strictEqual(dnaResult, false)
+      assert.strictEqual(dnaChecker.isSimian(dna), false)
     })
+
     it("should return false given a dna sequence with a nitrogen base that is not A, T, C or G ", function () {
       const dna = ["CCTM"]
-      const dnaResult = dnaChecker.isSimian(dna)
-      assert.strictEqual(dnaResult, false)
+      assert.strictEqual(dnaChecker.isSimian(dna), false)
     })
+    
   })
 })
