ef8af91e57554162b811cb4751aeb13c67d7654f
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-24 22:51:00 +0100

    refactor to change "chemical" name to "nitrogen"

--- test/dnaCheckerTest.js
+++ test/dnaCheckerTest.js
@@ -13,23 +13,28 @@ describe("SimiosOrHuman", function () {
       const dnaResult = dnaChecker.isSimian(dna)
       assert.strictEqual(dnaResult, false)
     })
-    it("should return false given a dna sequence with one chemical base", function () {
+    it("should return false given a dna sequence with one nitrogen base", function () {
       const dna = ["T"]
       const dnaResult = dnaChecker.isSimian(dna)
       assert.strictEqual(dnaResult, false)
     })
-    it("should return false given a dna sequence with two chemical bases", function () {
+    it("should return false given a dna sequence with two nitrogen bases", function () {
       const dna = ["TC"]
       const dnaResult = dnaChecker.isSimian(dna)
       assert.strictEqual(dnaResult, false)
     })
-    it("should return false given a dna sequence with three chemical bases", function () {
+    it("should return false given a dna sequence with three nitrogen bases", function () {
       const dna = ["TCA"]
       const dnaResult = dnaChecker.isSimian(dna)
       assert.strictEqual(dnaResult, false)
     })
-    it("should return false given a dna sequence with four different chemical bases", function () {
+    it("should return false given a dna sequence with four different nitrogen bases", function () {
       const dna = ["TCAG"]
+      const dnaResult = dnaChecker.isSimian(dna)
+      assert.strictEqual(dnaResult, false)
+    })
+    it("should return false given a dna sequence with a nitrogen base that is not A, T, C or G ", function () {
+      const dna = ["CCTM"]
       const dnaResult = dnaChecker.isSimian(dna)
       assert.strictEqual(dnaResult, false)
     })
