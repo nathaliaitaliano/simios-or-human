717edd53793c689cb819c963bd3906fdf3658cd4
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-25 01:14:41 +0100

    dnaValidator and checkNitrogenBasesSequence methods created dnaValidator to validate a dna that have a sequence with four or more nitrogen bases (A, T, C or G); checkNitrogenBasesSequence to check if a dna have a simian sequence included

--- dnaChecker.js
+++ dnaChecker.js
@@ -1,19 +1,29 @@
 const isSimian = dna => {
-  const nitrogenBasesPosibles = ["A", "T", "C", "G"]
-  let dnaResult = false
-
-  dna.forEach(sequence => {
-    // console.log(sequence)
-    const nitrogenBases = sequence.split("")
-    // console.log(nitrogenBases)
-    nitrogenBases.forEach(base => {
-      // console.log(base)
-      dnaResult = nitrogenBases.length >= 4 && nitrogenBasesPosibles.includes(base)
-    })
-  })
-  return dnaResult
+  return dnaValidator(dna) && checkNitrogenBasesSequence(dna)
 }
 
+const dnaValidator = dna => {
+  const nitrogenBasesPosibles = ["A", "T", "C", "G"]
+  let dnaValidation = false
 
+  dna.forEach(nitrogenBasesSequence => {
+    const nitrogenBases = nitrogenBasesSequence.split("")
+    nitrogenBases.forEach(base =>
+      dnaValidation = nitrogenBases.length >= 4 && nitrogenBasesPosibles.includes(base))
+  })
+  return dnaValidation
+}
+
+const checkNitrogenBasesSequence = dna => {
+  const sequencesToBeSimian = ["AAAA", "TTTT", "CCCC", "GGGG"]
+  let nitrogenBasesSimianSequence = false
+
+  dna.forEach(nitrogenBasesSequence => {
+    sequencesToBeSimian.forEach(simianSequence => {
+      if (nitrogenBasesSequence.indexOf(simianSequence) !== -1) return nitrogenBasesSimianSequence = true
+    })
+  })
+  return nitrogenBasesSimianSequence
+}
 
 module.exports = { isSimian }
