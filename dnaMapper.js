40419ef22eafd9a21a88ed7a09064af430664c6c
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-26 23:51:07 +0100

    method refactor to return dna sequences with left diagonals

--- dnaMapper.js
+++ dnaMapper.js
@@ -1,25 +1,45 @@
 const mapSequences = dna => {
-  const dnaSequences = []
+  const dnaSequences = dna.concat(mapVerticalSequences(dna), mapLeftDiagonalSequences(dna))
+  return dnaSequences
+}
+
+const mapVerticalSequences = dna => {
+  const verticalSequences = []
 
   for (let i = 0; i < dna.length; i++) {
     let verticalSequence = ""
     dna.forEach(sequence => {
       verticalSequence = verticalSequence.concat(sequence.charAt(i))
     })
-    dnaSequences.push(verticalSequence)
+    verticalSequences.push(verticalSequence)
   }
+
+  return verticalSequences
+}
+
+const mapLeftDiagonalSequences = dna => {
+  const leftDiagonalSequences = []
 
   for (let i = 0; i < dna.length; i++) {
-    let leftDiagonalSequence = dna[0].charAt(i)
+    let leftDiagonalSequenceHorizontal = dna[0].charAt(i)
     let nextPosition = i + 1
     while (nextPosition < dna.length) {
-      leftDiagonalSequence = leftDiagonalSequence.concat(dna[nextPosition].charAt(nextPosition))
+      leftDiagonalSequenceHorizontal = leftDiagonalSequenceHorizontal.concat(dna[nextPosition].charAt(nextPosition))
       nextPosition++
     }
-    dnaSequences.push(leftDiagonalSequence)
+    leftDiagonalSequences.push(leftDiagonalSequenceHorizontal)
   }
 
-  return dna.concat(dnaSequences)
+  for (let i = 1; i < dna.length; i++) {
+    let leftDiagonalSequenceVertical = dna[1].charAt(i - 1)
+    let nextPosition = i + 1
+    while (nextPosition < dna.length) {
+      leftDiagonalSequenceVertical = leftDiagonalSequenceVertical.concat(dna[nextPosition].charAt(nextPosition))
+      nextPosition++
+    }
+    leftDiagonalSequences.push(leftDiagonalSequenceVertical)
+  }
+  return leftDiagonalSequences
 }
 
 module.exports = { mapSequences }
