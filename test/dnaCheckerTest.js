081eaa135c6b1933735fcbb89066e21163f8f2ca
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-27 22:22:23 +0100

    test refactor to switch the positions for result expected and result actual

--- test/dnaCheckerTest.js
+++ test/dnaCheckerTest.js
@@ -5,22 +5,22 @@ describe("SimiosOrHuman", function () {
   describe("isSimian", function () {
     it("should return false given a empty dna input", function () {
       const dna = []
-      assert.strictEqual(false, dnaChecker.isSimian(dna))
+      assert.strictEqual(dnaChecker.isSimian(dna), false)
     })
 
     it("should return false given a empty dna sequence", function () {
       const dna = [""]
-      assert.strictEqual(false, dnaChecker.isSimian(dna))
+      assert.strictEqual(dnaChecker.isSimian(dna), false)
     })
 
     it("should return false given a dna sequence with four different nitrogen bases", function () {
       const dna = ["TCAG"]
-      assert.strictEqual(false, dnaChecker.isSimian(dna))
+      assert.strictEqual(dnaChecker.isSimian(dna), false)
     })
 
     it("should return false given a dna sequence with a nitrogen base that is not A, T, C or G ", function () {
       const dna = ["CCTM"]
-      assert.strictEqual(false, dnaChecker.isSimian(dna))
+      assert.strictEqual(dnaChecker.isSimian(dna), false)
     })
 
     it("should return true given a dna sequence with four followed equals nitrogen bases", function () {
@@ -30,7 +30,7 @@ describe("SimiosOrHuman", function () {
 
     it("should return false given a dna that not contains, at least, one sequence with four followedequals nitrogen bases", function () {
       const dna = ["TCGT", "AAAT", "CCTT", "GGTA"]
-      assert.strictEqual(false, dnaChecker.isSimian(dna))
+      assert.strictEqual(dnaChecker.isSimian(dna), false)
     })
 
     it("should return true given a dna that contains, at least, one nitrogen sequence with four followed equals nitrogen bases", function () {
