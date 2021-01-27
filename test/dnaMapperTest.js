fab8a35c0f93af7e3d0dd8bc688378b98220b944
nathaliaitaliano <nathalia.italiano@gmail.com>
2021-01-27 19:16:35 +0100

    test for a dna sequence with three nitrogen bases

--- test/dnaMapperTest.js
+++ test/dnaMapperTest.js
@@ -12,25 +12,32 @@ describe("SimiosOrHuman", function () {
       assert.ok(contains(dnaSequences, dnaSequencesExpected))
     })
 
-    it("should retturn the vertical dna sequences given a dna", function () {
+    it("should return the vertical dna sequences given a dna", function () {
       const dna = ["CT", "GC"]
       const dnaSequencesExpected = ["CC", "T"]
       const dnaSequences = dnaMapper.mapSequences(dna)
       assert.ok(contains(dnaSequences, dnaSequencesExpected))
     })
 
-    it("should retturn the left diagonals dna sequences given a dna", function () {
+    it("should return the left diagonals dna sequences given a dna", function () {
       const dna = ["CT", "GC"]
       const dnaSequencesExpected = ["G", "CC", "T"]
       const dnaSequences = dnaMapper.mapSequences(dna)
       assert.ok(contains(dnaSequences, dnaSequencesExpected))
     })
 
-    it("should retturn the right diagonals dna sequences given a dna", function () {
+    it("should return the right diagonals dna sequences given a dna", function () {
       const dna = ["CT", "GC"]
-      const dnaSequencesExpected = ["C", "TG", "C" ]
+      const dnaSequencesExpected = ["C", "TG", "C"]
       const dnaSequences = dnaMapper.mapSequences(dna)
       assert.ok(contains(dnaSequences, dnaSequencesExpected))
+    })
+
+    it("should return all posibles sequences (based on horizontal, vertical, left an right diagonal directions) given a dna", function () {
+      const dna = ["CAT", "GAC", "ATC"]
+      const dnaSequencesExpected = ["CAT", "GAC", "ATC", "CGA", "AAT", "TCC", "CAC", "AC", "T", "GT", "A", "TAA", "AG", "C", "CT", "C"]
+      const dnaSequences = dnaMapper.mapSequences(dna)
+      assert.deepStrictEqual(dnaSequencesExpected, dnaSequences)
     })
   })
 })
